44cb9511e9b5ab49151c9aae9d8d6fb81becce5f
Fernando Gutemberg <fernando70sil@gmail.com>
2022-07-11 20:08:35 -0300

    Meu Primeiro Porjeto App - parte 1

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  render(){
+
+    let nome = 'Fernando';
+
+    return(
+      <View>
+        <Text>Olá mundo</Text>
+        <Text>Meu Primeiro Projeto</Text>
+        <Text style={{ color: '#FF0000', fontSize: 25, margin: 15}}>
+          Sujeito Programador
+        </Text>
+
+        <Text style={{ fontSize: 30}}> {nome} </Text> 
+
+        <Jobs largura={200} altura={500} fulano="Steve Jobs" />
+
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+class Jobs extends Component{
+  render(){
+
+    let img = 'https://sujeitoprogramador.com/steve.png'
+
+    return(
+      <View>
+        <Image
+        source={{ uri: img }}
+        style={{ width: this.props.largura, height: this.props.altura }}
+        />
+        <Text>{this.props.fulano}</Text>
+
+      </View>
+   
+      
+    );
+  }
+}
+
